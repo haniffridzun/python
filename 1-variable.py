91f3466204c33eff5ceb03ce7a0d1c1845814b20
haniffridzun <haniffridzun@gmail.com>
2023-01-31 23:25:35 +0800

    Intro to python

---
+++ 1-variable.py
@@ -0,0 +1,18 @@
+# check the type of value
+print(type('hello world'))
+print(type(1 + 9))
+# output: <class 'str'>
+
+
+# variable is a name that refers to a value
+message = 'and now for something completely different'
+print(message)
+# variable name can contain letters and numbers
+# cannot start with number
+# begin with lowercase letter
+# start with underscore if writing library code for others to use
+
+
+# get user input
+inp = input('type something: ')
+print(inp)
